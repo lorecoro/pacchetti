b2734c085f63a39bbd43c93de1b6fdb1bd442ccf
Lorenzo <lorenzo@coronati.com>
2024-09-22 22:03:45 +0300

    prisma schema

---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { NextUIProvider } from "@nextui-org/react";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: Props) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  );
+}
