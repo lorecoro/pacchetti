c32fc6240ac0649adfb01bfe64f6b2bd0abd52ee
Lorenzo <lorenzo@coronati.com>
2024-10-31 21:59:03 +0200

    i18n setup

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Providers from '@/app/providers';
-import Sidenav from '@/components/common/sidebar';
+import Sidebar from '@/app/components/common/sidebar';
 
 const folks = localFont({
   src: "./fonts/Folks-Light.ttf.woff"
@@ -26,7 +26,7 @@ export default function RootLayout({
         <Providers>
           <main className="light light:bg-cyan-200 light:text-black bg-black text-white grid grid-cols-6 gap-4 p-4">
             <div className="grid col-span-1">
-              <Sidenav />
+              <Sidebar />
             </div>
             <div className="grid col-span-5">
               <div className="w-full border-medium px-4 py-4 rounded-small border-default-400 dark:border-default-100">
--- app/components/common/sidebar.tsx
+++ app/components/common/sidebar.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import paths from "@/paths";
-import User from "@/components/user/user";
+import User from "@/app/components/user/user";
 import { 
   ArchiveBoxIcon,
   BuildingOffice2Icon,
@@ -12,10 +12,12 @@ import {
   HomeIcon,
 } from '@heroicons/react/24/outline';
 import { isAdmin, isAuthenticated } from "@/actions/user";
+import { getTranslations } from "next-intl/server";
 
-export default async function Header() {
+export default async function Sidebar() {
   const admin: boolean = await isAdmin();
   const authenticated: boolean = await isAuthenticated();
+  const t = await getTranslations("Sidebar");
 
   return (
     <div className="w-full border-medium px-4 py-4 rounded-small border-default-400 dark:border-default-100">
@@ -26,7 +28,7 @@ export default async function Header() {
       {authenticated &&
       <div className="flex w-full max-w-full items-center justify-between rounded-lg py-1 px-3 text-zinc-950 dark:text-zinc-400">
         <HomeIcon className="w-6" />
-        <Link className="mr-auto text-large font-semibold text-zinc-950 dark:text-zinc-400 p-4" href={paths.home()}>Dashboard</Link>
+        <Link className="mr-auto text-large font-semibold text-zinc-950 dark:text-zinc-400 p-4" href={paths.home()}>{t("Dashboard")}</Link>
       </div>
       }
 
