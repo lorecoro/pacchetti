4024e8350e3feedb2ae0432886b3b89dc5d4e116
Lorenzo <lorenzo@coronati.com>
2026-01-05 10:19:38 +0200

    npm update, refactor paths.ts

--- paths.ts
+++ paths.ts
@@ -6,27 +6,13 @@ const paths = async () => {
   const locale = await getLocale();
   const prefix = locale.split('-')[0];
   return {
-    home() {
-      return '/';
-    },
-    adminCompanies() {
-      return `/${prefix}/admin/company/list`;
-    },
-    invoices() {
-      return `/${prefix}/invoice/list`;
-    },
-    packages() {
-      return '/package/list';
-    },
-    onePackage(id: string) {
-      return `/package/${id}`;
-    },
-    entries() {
-      return '/entry/list';
-    },
-    profile() {
-      return '/profile';
-    }
+    home: () => '/',
+    adminCompanies: () => `/${prefix}/admin/company/list`,
+    invoices: () => `/${prefix}/invoice/list`,
+    packages: () => '/package/list',
+    onePackage: (id: string) => `/package/${id}`,
+    entries: () => '/entry/list',
+    profile: () => '/profile'
   }
 };
 
